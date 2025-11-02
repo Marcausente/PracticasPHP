4af0fad7c350db6c35940fba0e7395c984e3541f
Marc Fernández <marcausente@gmail.com>
2025-11-02 21:03:53 +0100

    FEAT: implementado ex3

--- src/FORMS/class/libro.php
+++ src/FORMS/class/libro.php
@@ -1,59 +1,59 @@
 <?php
 
-class Libro {
-  public $titulo;
-  public $autor;
-  public $fecha_publicacion;
-  public $paginas;
+class Libro{
+public $titulo;
+public $autor;
+public $ano_publicacion;
+public $num_paginas;
 
-
-public function __construct($titulo, $autor, $fecha_publicacion, $paginas){
+public function __construct($titulo, $autor, $ano_publicacion, $num_paginas)
+{
   $this->titulo = $titulo;
   $this->autor = $autor;
-  $this->paginas = $paginas;
-  $this->fecha_publicacion = $fecha_publicacion;
+  $this->ano_publicacion = $ano_publicacion;
+  $this->num_paginas = $num_paginas;
 }
 
-// Getters
-  public function getTitulo() {
+  public function getTitulo()
+  {
     return $this->titulo;
   }
 
-  public function getAutor() {
-    return $this->autor;
-  }
-
-  public function getFechaPublicacion() {
-    return $this->fecha_publicacion;
-  }
-
-  public function getPaginas() {
-    return $this->paginas;
-  }
-
-  // Setters
-  public function setTitulo($titulo) {
+  public function setTitulo($titulo)
+  {
     $this->titulo = $titulo;
   }
 
-  public function setAutor($autor) {
+  public function setAutor($autor){
     $this->autor = $autor;
   }
 
-  public function setFechaPublicacion($fecha_publicacion) {
-    $this->fecha_publicacion = $fecha_publicacion;
+  public function getAutor(){
+    return $this->autor;
   }
 
-  public function setPaginas($paginas) {
-    $this->paginas = $paginas;
+  public function setAno_Publicacion($ano_publicacion){
+    $this->ano_publicacion = $ano_publicacion;
   }
 
-  public function datosLibro(){
-    return "Título: {$this->titulo}<br>
-            Autor: {$this->autor}<br>
-            Fecha de Publicación: {$this->fecha_publicacion}<br>
-            Páginas: {$this->paginas}";
-}
+  public function getAno_Publicacion(){
+    return $this->ano_publicacion;
+  }
+
+  public function setNum_Paginas($num_paginas){
+    $this->num_paginas = $num_paginas;
+  }
+
+  public function getNum_Paginas(){
+    return $this->num_paginas;
+  }
+
+
+
+
+
+
+
 }
 
 ?>
--- src/FORMS/ex3forms.php
+++ src/FORMS/ex3forms.php
@@ -1,15 +1,49 @@
 <?php
 require_once "class/libro.php";
 
-  $titulo = $_POST['titulo'] ?? '';
-  $autor = $_POST['autor'] ?? '';
-  $paginas = $_POST['paginas'] ?? '';
-  $fecha_publicacion = $_POST['fecha'] ?? '';
+if ($_SERVER["REQUEST_METHOD"] == "POST"){
+  $titulo = $_POST["titulo"];
+  $autor = $_POST["autor"];
+  $paginas = $_POST["paginas"];
+  $fecha = $_POST["fecha"];
+  $errores = [];
 
-  $libro = new Libro($titulo, $autor, $fecha_publicacion, $paginas);
+  $errores = verificacion($titulo, $autor, $paginas, $fecha, $errores);
 
-  echo ($libro->datosLibro());
+  if (empty($errores)){
+    $libro = new Libro($titulo, $autor, $paginas, $fecha);
 
+    echo $libro->getTitulo();
+    echo $libro->getAutor();
+    echo $libro->getNum_Paginas();
+    echo $libro->getAno_Publicacion();
+  }else{
+    for($i = 0; $i < count($errores); $i++){
+      echo $errores[$i];
+    }
+  }
+
+}else{
+  echo "Error al verificar los datos";
+}
+
+function verificacion($titulo, $autor, $paginas, $fecha, $errores){
+
+  if(empty($titulo) || empty($autor) || empty($paginas) || empty($fecha)){
+    $errores [] = "Debes rellenar los campos" . "<br><br>";
+  }
+
+  if (!is_numeric($paginas)){
+    $errores[] = "Paginas debe ser un numero" . "<br><br>";
+  }
+
+  if (!is_numeric($fecha)){
+    $errores[] = "Fecha debe ser un numero" . "<br><br>";
+  }
+
+  return $errores;
+
+}
 
 ?>
 
@@ -24,23 +58,23 @@ require_once "class/libro.php";
     <h1>Formulario de libros</h1>
     <form method="post" action="#" novalidate>
       <div class="field">
-        <label for="nombre">Titulo</label>
+        <label for="titulo">Titulo</label>
         <input type="text" id="titulo" name="titulo" placeholder="Titulo" autocomplete="given-name" required />
       </div>
 
       <div class="field">
-        <label for="apellido">Autor</label>
+        <label for="autor">Autor</label>
         <input type="text" id="autor" name="autor" placeholder="Autor" autocomplete="family-name" required />
       </div>
 
       <div class="field">
-        <label for="apellido">Paginas</label>
+        <label for="paginas">Paginas</label>
         <input type="num" id="paginas" name="paginas" placeholder="Paginas" autocomplete="family-name" required />
       </div>
 
       <div class="field">
-        <label for="apellido">Fecha Publicación</label>
-        <input type="date" id="fecha" name="fecha" placeholder="Fecha Publicación" autocomplete="family-name" required />
+        <label for="fecha">Fecha Publicación</label>
+        <input type="num" id="fecha" name="fecha" placeholder="Fecha Publicación" autocomplete="family-name" required />
       </div>
 
       <button type="submit">Enviar</button>
